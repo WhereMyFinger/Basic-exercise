7afd7983425484f2725b2648df4d149e2d00484c
nguyenle2412 <nle8144@gmail.com>
2022-06-13 17:18:56 +0700

    up bai 1-17

---
+++ part01/Exercise02.java
@@ -0,0 +1,27 @@
+package part01;
+
+public class Exercise02 {
+	
+	public static final String KHONG = "không";
+	public static final String MOT = "một";
+	public static final String HAI = "hai";
+	public static final String BA = "ba";
+	public static final String BON = "bốn";
+	public static final String NAM = "năm";
+	public static final String SAU = "sáu";
+	public static final String BAY = "bảy";
+	public static final String TAM = "tám";
+	public static final String CHIN = "chín";
+	public static final String MUOIF = "mười";
+    public static final String MUOI = "mươi";
+    public static final String LAM = "lăm";
+    public static final String LE = "lẻ";
+    public static final String MOTS = "mốt";
+    public static final String TRAM = "trăm";
+    public static final String NGHIN = "nghìn";
+    public static final String TRIEU = "triệu";
+    public static final String TY = "tỷ";
+    
+    
+
+}
